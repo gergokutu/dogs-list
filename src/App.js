34eb20d8daea09715ce57ba17d6f27df109c39e0
gergokutu <gergo.kutu@gmail.com>
2020-05-08 16:06:11 +0200

    Implement go back to index link in DogBreedImages, then render it in App on route /dog-breeds/:breed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import DogsListContainer from './components/DogsListContainer';
 import { Route } from 'react-router-dom';
+import DogBreedImages from './components/DogBreedImages';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={DogsListContainer} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImages} />
     </div>
   );
 }
